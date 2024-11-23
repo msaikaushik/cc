0c30db0cdffc6be5e0cb935d0fcf7880954c9238
Sai Kaushik <m.saikaushik97@gmail.com>
2024-11-23 06:14:36 +0000

    wc: arg selection

--- wc/src/ccwc.cpp
+++ wc/src/ccwc.cpp
@@ -8,15 +8,19 @@ int main(int argc, char *argv[]) {
 	string arg = argv[1];
 	string fileName = argv[2];
 	
-	ifstream file (fileName, ios::binary);
+	if (arg == "-c") {
+		ifstream file (fileName, ios::binary);
 	
-	if(!file.is_open()) {
-		cerr << "Error: Could not open file" << endl;
-		return 1;
-	}
+		if (!file.is_open()) {
+			cerr << "Error: Could not open file" << endl;
+			return 1;
+		}
 
-	file.seekg(0, ios::end);
-	int fileSize = file.tellg();
-	cout<<fileSize<<endl;
-	return 0;
+		file.seekg(0, ios::end);
+		int fileSize = file.tellg();
+		cout<<fileSize<<endl;
+		return 0;
+	}
+	cout<<"Invalid Arg"<<endl;
+	return 1;
 }
